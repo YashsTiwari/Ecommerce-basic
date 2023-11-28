f9537e486b240eee3eda58f7d7701ea56a34020a
Yashs Tiwari <yashstiwari03@gmail.com>
2023-11-28 22:40:30 +0530

    added redux toolkit functionality and status

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,58 @@
+import { useEffect } from "react";
+import CardTemplate from "./CardTemplate";
+import { useDispatch, useSelector } from "react-redux";
+import { add } from "../store/cartSlice";
+import { getProducts } from "../store/productSlice";
+import Loader from "../utils/loadingSpinner";
+import StatusCode from "../utils/statusCode";
+
 function Product() {
-  return (
-    <div>
-      <h1>Product Dashboard</h1>
+  const dispatch = useDispatch();
+  const { data: products, status } = useSelector((state) => state.products);
+
+  /*
+  async function fetchData() {
+    try {
+      const res = await fetch(" https://api.escuelajs.co/api/v1/products");
+      if (!res.ok) {
+        throw new Error("failed to fetch");
+      }
+      const data = await res.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error in fetching data: ", error);
+    }
+  }
+*/
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  if (status === StatusCode.LOADING) return <Loader />;
+  if (status === StatusCode.ERROR) {
+    return <p>Something went wrong! 🫤 Try again later...</p>;
+  }
+
+  const addToCart = (product) => {
+    dispatch(add(product));
+  };
+
+  const cards = products.map((product) => (
+    <div className="col-md-3" style={{ marginBottom: "10px" }}>
+      <CardTemplate
+        key={product.id}
+        product={product}
+        handleClick={addToCart}
+        buttonType={"primary"}
+        buttonText={"Add To Cart"}
+      />
     </div>
+  ));
+  return (
+    <>
+      <h1>Product Dashboard</h1>
+      <div className="row">{cards}</div>
+    </>
   );
 }
 
